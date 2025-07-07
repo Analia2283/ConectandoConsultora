5b4d84949ab52b846b0c07f230a2fed96130ee8a
Analia2283 <analiaisla@yahoo.com.ar>
2025-07-07 19:47:55 -0300

    corrigiendo enlaces

--- js/menu.js
+++ js/menu.js
@@ -1,36 +1,23 @@
-document.addEventListener("DOMContentLoaded", ()=>{    
+document.addEventListener("DOMContentLoaded", () => {
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
     const navLinks = document.querySelectorAll('.nav__links');
 
-    openButton.addEventListener('click', ()=>{
+    // Abre el menú cuando se hace clic en el botón de menú
+    openButton.addEventListener('click', () => {
         menu.classList.add('nav__link--show');
     });
 
-    closeMenu.addEventListener('click', ()=>{
+    // Cierra el menú cuando se hace clic en el botón de cerrar
+    closeMenu.addEventListener('click', () => {
         menu.classList.remove('nav__link--show');
     });
 
-
-});
-    navLinks.forEach(link => { 
-        link.addEventListener('click', () => { 
-            menu.classList.remove('nav__link--show'); 
-        })
-    navLinks.forEach(link => { 
-        link.addEventListener('click', () => { 
-            menu.classList.remove('nav__link--show'); 
-
-        })
-    navLinks.forEach(link => { 
-        link.addEventListener('click', () => { 
-            menu.classList.remove('nav__link--show'); 
-
+    // Cierra el menú cuando se hace clic en cualquiera de los enlaces de navegación
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('nav__link--show');
         });
     });
-
-    
-    })
-    
 });
